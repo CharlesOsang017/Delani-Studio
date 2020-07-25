ae0152468e0f7720810d1f116a1612a8f5317b2d
CharlesOsango17 <charlesosango02@gmail.com>
2020-07-25 18:19:14 +0300

    Added a hover to the portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,53 @@
-$(document).ready(function(){
-    $("#img").click(function(){
+$(document).ready(function () {
+    $("#img").click(function () {
         $("#p").show();
         $("#img").hide();
     });
-    $("#p").click(function(){
+    $("#p").click(function () {
         $("#img").show();
         $("#p").hide();
     });
-    $("#img1").click(function(){
+    $("#img1").click(function () {
         $("#p1").show();
         $("#img1").hide();
-     });
-     $("#p1").click(function(){
-         $("#img1").show();
-         $("#p1").hide();
-     });
-     $("#img2").click(function(){
-         $("#p2").show();
-         $("#img2").hide();
-     });
-     $("#p2").click(function(){
-         $("#img2").show();
-         $("#p2").hide();
-     });
-     
+    });
+    $("#p1").click(function () {
+        $("#img1").show();
+        $("#p1").hide();
+    });
+    $("#img2").click(function () {
+        $("#p2").show();
+        $("#img2").hide();
+    });
+    $("#p2").click(function () {
+        $("#img2").show();
+        $("#p2").hide();
+    });
+
+    //Creating a hover on the portfolio picture
+
+    $('img#port1').hover(function () {
+        $('#port1').slideToggle('slow');
+    });
+    $('img#port2').hover(function () {
+        $('#port2').slideToggle('slow');
+    });
+    $('img#port3').hover(function () {
+        $('#port3').slideToggle('slow');
+    });
+    $('img#port4').hover(function () {
+        $('#port4').slideToggle('slow');
+    });
+    $('img#port5').hover(function () {
+        $('#port5').toggle('slow');
+    });
+    $('img#port6').hover(function () {
+        $('#port6').toggle('slow');
+    });
+    $('img#port7').hover(function () {
+        $('#port7').toggle('slow');
+    });
+    $('p#port8').hover(function () {
+        $('#port8').slideToggle('slow');
+    });
 });
